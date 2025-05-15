fa6e8f146607713921f10235493d63dc3d859685
astankovic <aleksa.stankovic@berkeley.edu>
2025-05-15 12:07:34 +0200

    Add address validation to the addressFormValidator.

--- frontend/src/components/AddressForm/utils/addressFormValidator.ts
+++ frontend/src/components/AddressForm/utils/addressFormValidator.ts
@@ -1,13 +1,15 @@
+import { isAddress } from 'viem'
 import { AddressFormValues } from '../types/AddressForm.types'
 
-export const addressFormValidator = (
-  values: AddressFormValues
-) => {
+export const addressFormValidator = (values: AddressFormValues) => {
   const errors: {
     address?: string
   } = {}
 
   if (!values.address) errors.address = 'Required'
+  if (values.address && !isAddress(values.address)) {
+    errors.address = 'Invalid address format'
+  }
 
   return errors
 }
