67cb1d9caea393dcbcbff016ba6d2fec721ceed4
astankovic <aleksa.stankovic@berkeley.edu>
2025-05-15 15:25:06 +0200

    Add initial balance endpoint with native token fetching.

---
+++ backend/src/constants/general.constants.ts
@@ -0,0 +1,2 @@
+export const NATIVE_TOKEN = 'ETH'
+export const NATIVE_TOKEN_DECIMALS = 18
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,49 @@
 import express from 'express'
+import { Address, createPublicClient, formatUnits, http, isAddress } from 'viem'
+import { mainnet } from 'viem/chains'
+
+import {
+  NATIVE_TOKEN,
+  NATIVE_TOKEN_DECIMALS,
+} from './constants/general.constants.js'
 
 const app = express()
 const port = '3000'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+export const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
+app.get('/api/balance/:address', async (req, res) => {
+  const { address } = req.params
+
+  if (!isAddress(address)) {
+    res.status(400).json({ error: 'Invalid Ethereum address' })
+  }
+
+  try {
+    const formattedAddress = address as Address
+
+    // Object to store token balances
+    const balances: Record<string, string> = {}
+
+    const nativeTokenBalance = await publicClient.getBalance({
+      address: formattedAddress,
+    })
+
+    balances[NATIVE_TOKEN] = formatUnits(
+      nativeTokenBalance,
+      NATIVE_TOKEN_DECIMALS
+    ).toString()
+
+    res.json({
+      balances,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
 })
 
 app.listen(port, () => {
