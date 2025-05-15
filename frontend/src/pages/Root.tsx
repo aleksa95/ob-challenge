8d44f5de4960cffcdbb3f0e54742baf2a6ba64e0
astankovic <aleksa.stankovic@berkeley.edu>
2025-05-15 11:39:43 +0200

    Add logo and app name to the Root component.

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,5 +1,12 @@
+import logo from '@/assets/logo.png'
+
 export const Root = () => (
-  <div className="flex flex-1 grow items-center justify-center p-5 max-sm:items-start">
-    root
+  <div className="flex flex-1 grow items-start p-10 justify-center max-sm:items-start">
+    <div className="flex gap-5 items-center">
+      <img src={logo} alt="logo" className="w-[82px] h-[82px]" />
+      <div className="font-title text-3xl font-bold text-white max-w-[151px]">
+        {import.meta.env.VITE_APP_NAME}
+      </div>
+    </div>
   </div>
 )
